9a52d981dba4171ef8b25151812d5ad26c20ed78
Victor De Camilo <victor.camilo@finnet.com.br>
2025-06-25 19:02:54 -0300

    first-commit-with-scaffold

---
+++ cypress.config.js
@@ -0,0 +1,19 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: "Cypress - Assignment",
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
+  retries: { runMode: 2 },
+  watchForFileChanges: false,
+  e2e: {
+    setupNodeEvents(on, config) {
+      require("cypress-mochawesome-reporter/plugin")(on);
+    },
+  },
+});
---
+++ cypress/e2e/frontendTests.cy.js
@@ -0,0 +1,11 @@
+describe("template spec", () => {
+  it("passes", () => {
+    cy.visit("https://example.cypress.io");
+  });
+});
+
+//Adicionar um produto na lista de compra como usuário comum
+
+//Limpar lista de compras como usuário comum
+
+//Cadastrar novo produto como usuário administrador
