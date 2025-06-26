f3d59e26c15a609958adc55d1ad09b3cbe300d47
Victor De Camilo <victor.camilo@finnet.com.br>
2025-06-26 10:54:17 -0300

    github-actions

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "z9cbmr",
   reporter: "cypress-mochawesome-reporter",
   reporterOptions: {
     charts: true,
